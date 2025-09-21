edc698b74a67cf7f7b55ad820e92bbc66012011a
Harshita-Poojary <harshita.poojary01@gmail.com>
2025-09-21 14:14:07 +0530

    Angular project set up

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,23 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
+
+  it('should create the app component', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const appComponent = fixture.componentInstance;
+    expect(appComponent).toBeTruthy();
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, temp-app');
+  });
+});
